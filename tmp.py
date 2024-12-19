da7d0bb3f6fe7c851db9135684c749c867996b48
Shawdox <478956370@qq.com>
2024-12-19 09:41:27 +0800

    Update Assign2Ternary

--- tmp.py
+++ tmp.py
@@ -1,13 +1,6 @@
-def f(char_map, text):
-    new_text = ('' + '')
-    idx_nkc3 = (88 + -88)
-    list_0euy = text
-    while idx_nkc3 < len(list_0euy):
-        ch = list_0euy[idx_nkc3]
-        val = char_map.get(ch)
-        if not val is None and (((True or False) and (False or True)) and ((8 > 5) or (8 < 5))):
-            new_text += val
-        else:
-            new_text += ch
-        idx_nkc3 = idx_nkc3 + (74 + -73)
-    return new_text
+def f(st):
+    if st[0] == '~':
+        e = 0 if 10 < 9 else st.rjust(10, 's')
+        return f(e)
+    else:
+        return st.rjust(10, 'n')
